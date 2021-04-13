cc1ef0f77336a7a43ec047c088ec6da87afd3b54
Nevilston <82449413+Nevilston@users.noreply.github.com>
2021-04-13 17:24:26 +0530

    Add files via upload

---
+++ String_length.java
@@ -0,0 +1,9 @@
+
+public class String_length {
+	public static void main(String args[]) {
+		
+	String s1 ="Javapoint";
+	String s2 = "Java";
+	System.out.println("String lenght is:"+s1.length());
+	System.out.println("String lenght is:"+s2.length());
+}}
