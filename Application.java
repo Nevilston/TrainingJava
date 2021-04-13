1077dca2cd40f71a2e496baf09a60ce7df87df19
Nevilston <82449413+Nevilston@users.noreply.github.com>
2021-04-13 17:17:37 +0530

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,28 @@
+/*
+public class Application {
+	public static void main(String[] args) {
+		int a=10;
+		int b=20;
+		int result = add(a,b);
+		System.out.println(result);
+	}
+public static int add(int a,int b) {
+	int c = a+b;
+	return c;
+}
+}
+*/
+public class FindSquareRoot{
+	public static int square(int a) {
+		int resullt = a*a;
+		return result;
+	}
+}
+public class Application {
+	public static void main(String[] args) {
+		double result = Math.sqrt(result);
+		System.out.println(result);
+		
+		
+	}
+}
